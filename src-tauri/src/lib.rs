0f3da2cad4c8df166fbefc100b844873710f7e64
mengen.dai <mengen.dai@outlook.com>
2025-04-02 22:38:41 +0800

    feat: 添加音频设备信息获取功能
    - 在 greet 函数中添加了音频设备信息获取逻辑
    - 引入 log 和 cpal依赖
    - 添加日志插件并配置

--- src-tauri/src/lib.rs
+++ src-tauri/src/lib.rs
@@ -1,13 +1,42 @@
+use cpal::traits::{DeviceTrait, HostTrait};
+use log::info;
+use tauri_plugin_log::{Target, TargetKind};
+
 // Learn more about Tauri commands at https://tauri.app/develop/calling-rust/
 #[tauri::command]
 fn greet(name: &str) -> String {
-    format!("Hello, {}! You've been greeted from Rust!", name)
+    let host = cpal::default_host();
+    let device = host
+        .default_input_device()
+        .expect("No input device available");
+    let config = device
+        .default_input_config()
+        .expect("Failed to get default input config");
+    let device_name = device.name().unwrap();
+    let sample_rate = config.sample_rate().0;
+    let channels = config.channels();
+    info!("channels: {}", channels);
+    info!("sample_rate: {}", sample_rate);
+    info!("device_name: {}", &device_name);
+    format!(
+        "Hello, {}! You've been greeted from Rust!, {}, {}, {}",
+        name, &device_name, sample_rate, channels
+    )
 }
 
 #[cfg_attr(mobile, tauri::mobile_entry_point)]
 pub fn run() {
     tauri::Builder::default()
         .plugin(tauri_plugin_opener::init())
+        .plugin(
+            tauri_plugin_log::Builder::new()
+                .targets([
+                    Target::new(TargetKind::Stdout),
+                    Target::new(TargetKind::LogDir { file_name: None }),
+                    Target::new(TargetKind::Webview),
+                ])
+                .build(),
+        )
         .invoke_handler(tauri::generate_handler![greet])
         .run(tauri::generate_context!())
         .expect("error while running tauri application");
--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,22 @@
 import "./App.scss";
-// import {invoke} from "@tauri-apps/api/core";
+import {useEffect, useState} from "react";
+import {invoke} from "@tauri-apps/api/core";
 import {DesktopApp} from "@src/components/desktop-app/desktop-app.tsx";
 
 function App() {
+
+    const [greetMsg, setGreetMsg] = useState("");
+    // const [name, setName] = useState("");
+
+    async function greet() {
+        // Learn more about Tauri commands at https://tauri.app/develop/calling-rust/
+        setGreetMsg(await invoke("greet", {name: "dai"}));
+        console.log(greetMsg)
+    }
+
+    useEffect(() => {
+        greet().then();
+    });
 
     return (
         <DesktopApp/>

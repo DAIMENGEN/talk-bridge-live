179daa5423586fc4752bf8679c5ebc8c190d472e
mengen.dai <mengen.dai@outlook.com>
2025-04-04 19:34:57 +0800

    feat(welcome): 更新欢迎页面样式和文案
    - 调整按钮宽度，从 200px 缩小到 100px
    - 更新欢迎页面的标题和内容文案，增加英文描述
    - 在欢迎页面添加 START 和 CLOSE 两个按钮

--- src/components/desktop-app/welcome/welcome.tsx
+++ src/components/desktop-app/welcome/welcome.tsx
@@ -7,15 +7,16 @@ export const Welcome = () => {
             <div className={"desktop-welcome-content"}>
                 <Space direction="vertical" size="small">
                     <h1>Talk Bridge Live</h1>
-                    <h2>跨越语言障碍的 AI 同声翻译软件</h2>
+                    <h2>AI Simultaneous Interpretation – Breaking Language Barriers</h2>
                     <h3>
-                        Talk Bridge Live 是一款由 Advantest 精心打造的 AI 同声翻译软件，专为企业会议、国际交流及多语言沟通场景设计。
-                        它依托先进的人工智能与实时语音识别技术，能够精准、流畅地将演讲者的语音即时翻译成目标语言，并以字幕或语音的方式呈现，让不同语言的与会者无缝沟通，仿佛置身同一间会议室。
-                        Talk Bridge Live 让语言不再成为沟通的障碍，让企业会议更加高效、顺畅。无论是跨国团队协作、行业论坛，还是国际商务洽谈，都能助您轻松实现零距离交流！
+                        <span>
+                            Talk Bridge Live is an AI simultaneous translation software meticulously developed by Advantest China R&D, designed for corporate meetings, international exchanges, and multilingual communication scenarios.
+                        </span>
                     </h3>
-                    <div className={"desktop-welcome-content-button"}>
-                        <Button size={"large"} >开始使用</Button>
-                    </div>
+                    <Space size={"large"} className={"desktop-welcome-content-button"}>
+                        <Button size={"large"}>START</Button>
+                        <Button size={"large"}>CLOSE</Button>
+                    </Space>
                 </Space>
             </div>
         </div>

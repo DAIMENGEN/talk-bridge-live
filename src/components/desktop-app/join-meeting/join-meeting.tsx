570480672f109684beab17c2da5bc8768cd077ac
mengen.dai <mengen.dai@outlook.com>
2025-05-12 21:00:46 +0800

    feat(join-meeting): 添加会议 ID 输入功能并进行有效性检查
    - 使用 React 的 useState 钩子来管理会议 ID 的状态
    - 在用户点击"Next"按钮时检查会议 ID 是否为空
    - 如果会议 ID 为空，显示警告消息提示用户输入
    - 优化了用户界面，提高了用户体验

--- src/components/desktop-app/join-meeting/join-meeting.tsx
+++ src/components/desktop-app/join-meeting/join-meeting.tsx
@@ -1,11 +1,14 @@
 import "./join-meeting.scss";
-import {Button, Input, Space} from "antd";
+import {useState} from "react";
 import {useNavigate} from "react-router-dom";
+import {Button, Input, Space, message } from "antd";
 import {InternationalIcon} from "@src/icons/international/international-icon.tsx";
 import {LegalNotice} from "@src/components/desktop-app/legal-notice/legal-notice.tsx";
 
 export const JoinMeeting = () => {
     const navigate = useNavigate();
+    const [meetingId, setMeetingId] = useState("");
+    const [messageApi, contextHolder] = message.useMessage();
     return (
         <div className={"join-meeting"}>
             <Space direction={"vertical"} size={"large"}>
@@ -15,13 +18,19 @@ export const JoinMeeting = () => {
                 <Space direction={"vertical"} size={"middle"}>
                     <Space direction={"vertical"} className={"join-meeting-content-input"} size={"small"}>
                         <div className={"input-label"}>Meeting ID</div>
-                        <Input placeholder={"Enter meeting ID"} defaultValue={"123456"}/>
+                        <Input placeholder={"Enter meeting ID"} onChange={e => setMeetingId(e.target.value)} defaultValue={meetingId}/>
                     </Space>
-                    <Button shape="round" size={"middle"}>Next</Button>
+                    <Button shape="round" size={"middle"} onClick={async () => {
+                        if (meetingId.length === 0) {
+                            await messageApi.warning("Please enter a meeting ID");
+                            return;
+                        }
+                    }}>Next</Button>
                     <Button shape="round" size={"middle"} onClick={() => navigate("/welcome")}>Previous</Button>
                 </Space>
             </Space>
             <LegalNotice/>
+            {contextHolder}
         </div>
     )
 }

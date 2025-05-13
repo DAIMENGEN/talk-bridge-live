b09bc02b39a4b00e0ac445e2dcedb2323e7f185b
mengen.dai <mengen.dai@outlook.com>
2025-05-13 20:47:58 +0800

    feat(meeting-room): 添加浮窗按钮的控制功能
    - 在 MeetingRoom 组件中添加 showFloatButtons 状态
    - 使用 useState钩子管理浮窗按钮的显示和隐藏
    - 在 FloatButton.Group 中添加 open 属性和 onClick事件处理

--- src/components/desktop-app/meeting-room/meeting-room.tsx
+++ src/components/desktop-app/meeting-room/meeting-room.tsx
@@ -3,13 +3,17 @@ import {useParams} from "react-router-dom";
 import {MoreIcon} from "@src/icons/more/more-icon.tsx";
 import {FloatButton} from "antd";
 import {SettingsFloatButton} from "@src/components/float-buttons";
+import {useState} from "react";
 
 export const MeetingRoom = () => {
-    const { meetingId } = useParams();
+    const {meetingId} = useParams();
+    const [showFloatButtons, setShowFloatButtons] = useState(false);
     return (
         <div className={"meeting-room"}>
             <h1>Meeting ID: {meetingId}</h1>
             <FloatButton.Group trigger={"click"}
+                               open={showFloatButtons}
+                               onClick={() => setShowFloatButtons(v => !v)}
                                icon={<MoreIcon width={20} height={20} color={"#141414"}/>}>
                 <SettingsFloatButton/>
             </FloatButton.Group>
